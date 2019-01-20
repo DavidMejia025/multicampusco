58478ed0f7b587adc17836006a4deb81cd1fcf63
David Mejia <davidndy@gmail.com>
2019-01-20 15:08:35 -0500

    improve about us, add visitors counters

--- pending_tasks.rb
+++ pending_tasks.rb
@@ -2,6 +2,8 @@
 
 Done. 1. Form
 2. Google adwords
-3. Nav bar
+done Se puede mejorar fijo en toda la pagina3. Nav bar
 Se puede mejorar sibiendo el <p> y bajando el <h1> 4. Acerca de nosotros: justificar y separar textos
-5. Color letra boton enviar)
+5. Color letra boton enviar
+6. responsive
+done 7. visitors count)
