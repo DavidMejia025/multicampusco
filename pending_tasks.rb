00b0633aca5ff5c68db90febfd2341bef496c4d4
David Mejia <davidndy@gmail.com>
2019-01-20 13:53:33 -0500

    redirect contact form, fix about us section

---
+++ pending_tasks.rb
@@ -0,0 +1,7 @@
+%q(Pending tasks:
+
+Done. 1. Form
+2. Google adwords
+3. Nav bar
+Se puede mejorar sibiendo el <p> y bajando el <h1> 4. Acerca de nosotros: justificar y separar textos
+5. Color letra boton enviar)
