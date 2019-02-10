1ce6f33bbb28086992c51522e8a0f8152fefcead
David Mejia <davidndy@gmail.com>
2019-02-09 22:29:37 -0500

    add pending tasks, modify counter display

--- pending_tasks.rb
+++ pending_tasks.rb
@@ -3,7 +3,14 @@
 Done. 1. Form
 2. Google adwords
 done Se puede mejorar fijo en toda la pagina3. Nav bar
-Se puede mejorar sibiendo el <p> y bajando el <h1> 4. Acerca de nosotros: justificar y separar textos
+done4. Acerca de nosotros: justificar y separar textos
 5. Color letra boton enviar
 6. responsive
-done 7. visitors count)
+done 7. visitors count.
+
+1. Google adwords
+2. nav bar fijo en toda la pagina y revisar el link a about us
+3. Color letra boton enviar
+4. responsive
+5. Front end del nav bar
+)
